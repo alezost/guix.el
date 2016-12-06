9b04eb08845a93466ff6481c3ed2e8389dd17bc7
Alex Kost <alezost@gmail.com>
2016-12-06 23:07:13 +0300

    elisp: Add 'guix-help' command
    * elisp/guix-about.el (guix-help-buffer-name, guix-help-doc-column)
    (guix-help-specifications): New variables.
    (guix-help-heading): New face.
    (guix-insert-info-button, guix-insert-doc-button)
    (guix-insert-command-button, guix-help-insert-specification)
    (guix-help-revert, guix-help-insert-content): New procedures.
    (guix-help): New command.
    * doc/emacs-guix.texi (Introduction): Mention it.
    * README: Likewise.

--- elisp/guix-about.el
+++ elisp/guix-about.el
@@ -24,6 +24,8 @@
 
 ;;; Code:
 
+(require 'bui)
+(require 'guix-utils)
 (require 'guix-config)
 
 (declare-function guix-eval-read "guix-repl" (str))
@@ -38,6 +40,176 @@
            (guix-eval-read "(@ (guix config) %guix-version)")
            guix-config-name
            guix-config-version))
+
+
+;;; "Guix Help" buffer
+
+(guix-define-groups help
+  :group-doc "Settings for '\\[guix-help]'."
+  :faces-group-doc "Faces for '\\[guix-help]'.")
+
+(defcustom guix-help-buffer-name "*Guix Help*"
+  "Buffer name for '\\[guix-help]'."
+  :type 'string
+  :group 'guix-help)
+
+(defcustom guix-help-doc-column 40
+  "Column at which 'doc' button is inserted."
+  :type 'integer
+  :group 'guix-help)
+
+(defface guix-help-heading
+  '((t :inherit bui-info-heading))
+  "Face for headings in `guix-help-buffer-name' buffer."
+  :group 'guix-help-faces)
+
+(defvar guix-help-specifications
+  '("Show packages"
+    guix-all-available-packages
+    guix-newest-available-packages
+    guix-installed-user-packages
+    guix-installed-system-packages
+    guix-installed-packages
+    guix-obsolete-packages
+    guix-packages-by-name
+    guix-packages-by-license
+    guix-packages-by-location
+    guix-package-from-file
+    guix-search-by-name
+    guix-search-by-regexp
+
+    "Show profile generations"
+    guix-generations
+    guix-last-generations
+    guix-generations-by-time
+    guix-system-generations
+    guix-last-system-generations
+    guix-system-generations-by-time
+
+    "Show/browse package licenses"
+    guix-licenses
+    guix-browse-license-url
+    guix-find-license-definition
+
+    "Show/find package locations"
+    guix-locations
+    guix-find-location
+    guix-edit
+
+    "Magit-like interface"
+    guix
+
+    "Show Hydra builds and jobsets"
+    guix-hydra-latest-builds
+    guix-hydra-queued-builds
+    guix-hydra-jobsets
+
+    "Hide hash parts in \"/gnu/store/…-foo\" file names"
+
+    (guix-prettify-mode t)
+    global-guix-prettify-mode
+
+    "Highlighting for package build logs"
+    (guix-build-log-mode t)
+    (guix-build-log-minor-mode t)
+
+    "Highlighting for Guix .scm files"
+    (guix-devel-mode t)
+
+    "Miscellaneous commands"
+    guix-emacs-autoload-packages ; available in Emacs installed with Guix
+    guix-set-current-profile
+    guix-pull
+    guix-apply-manifest
+    guix-version)
+  "List of command specifications for '\\[guix-help]'.
+Each specification can have one of the following forms:
+
+  TITLE
+  COMMAND-NAME
+  (COMMAND-NAME NO-BUTTON?)
+
+TITLE is a string; COMMAND-NAME is a symbol; NO-BUTTON? is a
+boolean value, that defines whether COMMAND-NAME is buttonized or
+not.")
+
+(defun guix-insert-info-button (label info-node)
+  "Insert button with LABEL to open texinfo manual.
+INFO-NODE is the name passed to `info' function."
+  (bui-insert-button
+   label 'button
+   'action (lambda (button)
+             (info (button-get button 'node)))
+   'node info-node))
+
+(defun guix-insert-doc-button (label symbol)
+  "Insert button with LABEL to open the docstring of SYMBOL."
+  (bui-insert-button
+   label 'button
+   'help-echo (format "Display documentation of '%S'" symbol)
+   'action (lambda (button)
+             (describe-symbol (button-get button 'symbol)))
+   'symbol symbol))
+
+(defun guix-insert-command-button (command)
+  "Insert button to run 'M-x COMMAND'."
+  (let ((command-string (symbol-name command)))
+    (bui-insert-button
+     command-string 'button
+     'help-echo (format "Call 'M-x %s'" command-string)
+     'action (lambda (button)
+               (call-interactively (button-get button 'command)))
+     'command command)))
+
+(defun guix-help-insert-specification (spec)
+  "Insert command specification SPEC at point.
+See `guix-help-specifications' for the meaning of SPEC."
+  (pcase spec
+    ((pred symbolp)
+     (guix-help-insert-specification (list spec nil)))
+    ((pred stringp)
+     (bui-newline)
+     (bui-format-insert spec 'guix-help-heading)
+     (bui-newline 2))
+    (`(,name ,no-command-button?)
+     (when (fboundp name)
+       (bui-with-indent bui-indent
+         (if no-command-button?
+             (insert (symbol-name name))
+           (guix-insert-command-button name))
+         (indent-to guix-help-doc-column 2)
+         (guix-insert-doc-button "doc" name))
+       (bui-newline)))
+    (_
+     (insert "<unknown specification>")
+     (bui-newline))))
+
+(defun guix-help-revert (_ignore-auto noconfirm)
+  "Revert function for `revert-buffer-function'."
+  (when (or noconfirm
+            (y-or-n-p (format "Revert %s buffer? " (buffer-name))))
+    (guix-help-insert-content)))
+
+(defun guix-help-insert-content ()
+  "Insert Emacs-Guix help info into the current buffer."
+  (let ((inhibit-read-only t))
+    (erase-buffer)
+    (guix-insert-info-button "GNU Guix Manual" "guix")
+    (bui-newline)
+    (guix-insert-info-button "Emacs-Guix Manual" "emacs-guix")
+    (bui-newline 2)
+    (insert "Summary of the available M-x commands:\n")
+    (mapc #'guix-help-insert-specification
+          guix-help-specifications)))
+
+;;;###autoload
+(defun guix-help ()
+  "Display a summary of the available Emacs-Guix commands."
+  (interactive)
+  (with-output-to-temp-buffer guix-help-buffer-name
+    (set-buffer guix-help-buffer-name)
+    (setq-local revert-buffer-function 'guix-help-revert)
+    (guix-help-insert-content)))
 
 (provide 'guix-about)
 
