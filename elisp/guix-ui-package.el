64b598ef1832aa79e44ca9f2a83d0bac2d0b5af4
Alex Kost <alezost@gmail.com>
2017-01-25 11:34:41 +0300

    elisp/ui-package: Add "Size" buttons for installed outputs
    * elisp/guix-ui-package.el (guix-installed-output-info-insert-file-name):
    New procedure.
    (guix-installed-output-info-format): Use it.

--- elisp/guix-ui-package.el
+++ elisp/guix-ui-package.el
@@ -229,7 +229,7 @@ ENTRIES is a list of package entries to get info about packages."
   :required '(id name version installed non-unique))
 
 (bui-define-interface guix-installed-output info
-  :format '((file-name simple (indent bui-file))
+  :format '((file-name simple guix-installed-output-info-insert-file-name)
             (dependencies simple (indent bui-file)))
   :titles '((file-name . "Store directory"))
   :reduced? t)
@@ -522,6 +522,19 @@ current OUTPUT is installed (if there is such output in
     (bui-newline)
     (when installed-entry
       (bui-info-insert-entry installed-entry 'guix-installed-output 2))))
+
+(defun guix-installed-output-info-insert-file-name (file-name &optional _)
+  "Insert package store FILE-NAME at point."
+  (bui-insert-non-nil file-name
+    (bui-info-insert-value-indent file-name 'bui-file)
+    (bui-insert-indent)
+    (bui-insert-action-button
+     "Size"
+     (lambda (btn)
+       (guix-package-size (button-get btn 'file-name)
+                          (guix-read-package-size-type)))
+     "View package size of the current store file name"
+     'file-name file-name)))
 
 (defun guix-package-info-insert-action-button (type entry output)
   "Insert button to process an action on a package OUTPUT at point.
