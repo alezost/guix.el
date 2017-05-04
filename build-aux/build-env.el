2a541005cfed04522651b97c363485781cf5a4ec
Alex Kost <alezost@gmail.com>
2017-05-04 23:25:52 +0300

    build-env.el: Remove unneeded variable
    * build-aux/build-env.el (guix-env): Do not set
    'guix-package-enable-at-startup'.  This is not needed since
    <http://git.savannah.gnu.org/cgit/guix.git/commit/?id=9bd94544be43b7d028e1aaeb4069b07bdb24610f>.

--- build-aux/build-env.el
+++ build-aux/build-env.el
@@ -7,9 +7,6 @@
 ;; during compilation, so it is not necessary to specify
 ;; '--with-XXX-lispdir' options to "configure" script.
 (let ((guix-env (getenv "GUIX_ENVIRONMENT")))
-  ;; XXX `guix-package-enable-at-startup' should be removed from
-  ;; "guix-emacs.el" soon.
-  (setq guix-package-enable-at-startup nil)
   (when (and guix-env
              (require 'guix-emacs nil t))
     (guix-emacs-autoload-packages guix-env)))
