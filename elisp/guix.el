6d4ccb3f7376d6326b58b25f3c94ab546418a030
Alex Kost <alezost@gmail.com>
2017-07-02 17:24:20 +0300

    Update version to 0.3.2

--- elisp/guix.el
+++ elisp/guix.el
@@ -3,7 +3,7 @@
 ;; Copyright © 2016–2017 Alex Kost <alezost@gmail.com>
 
 ;; Author: Alex Kost <alezost@gmail.com>
-;; Version: 0.3.1
+;; Version: 0.3.2
 ;; URL: https://alezost.github.io/guix.el/
 ;; Keywords: tools
 ;; Package-Requires: ((emacs "24.3") (dash "2.11.0") (geiser "0.8") (bui "1.1.0") (magit-popup "2.1.0"))
