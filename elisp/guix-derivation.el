alezost/guix.el

commit 7bc54679d148de866af56a0dc5ba1d02817f0eba

elisp/derivation: Buttonize all store file names
* elisp/guix-derivation.el (guix-derivation-file-regexp): Adjust for all
file names.
(guix-derivation-file-regexp-group): New variable.
(guix-derivation-button): Use it.
(guix-derivation-drv-file-name): New face.
(guix-derivation-file-name-faces): New variable.
(guix-derivation-file-name-face): New function.
(guix-derivation-make-buttons): Adjust to use specialized faces.

elisp/guix-derivation.el
@@ -25,6 +25,7 @@
 
 ;;; Code:
 
+(require 'cl-lib)
 (require 'guix-utils)
 
 (defgroup guix-derivation nil
@@ -42,19 +43,49 @@
   "Face for store file names."
   :group 'guix-derivation-faces)
 
+(defface guix-derivation-drv-file-name
+  '((default :inherit guix-derivation-file-name)
+    (((class color) (background light)) :foreground "SpringGreen4")
+    (((class color) (background dark)) :foreground "SpringGreen3"))
+  "Face for '*.drv' store file names."
+  :group 'guix-derivation-faces)
+
 (defcustom guix-derivation-file-regexp
-  (rx-to-string `(and "/gnu/store/" (regexp ,guix-hash-regexp)
-                      (+ (any "-_+." alnum)) ".drv")
-                t)
+  (rx "\""
+      (group "/gnu/store/" (+ (not (any "\""))))
+      "\"")
   "Regexp matching Guix derivation file name."
   :type  'regexp
   :group 'guix-derivation)
 
+(defcustom guix-derivation-file-regexp-group 1
+  "Regexp group in `guix-derivation-file-regexp'."
+  :type 'integer
+  :group 'guix-derivation)
+
 (define-button-type 'guix-derivation-file
   'follow-link t
-  'face        'guix-derivation-file-name
+  'face        nil
   'help-echo   "Visit this file"
   'action      #'guix-derivation-button)
+
+(defvar guix-derivation-file-name-faces
+  '(("\\.drv\\'" . guix-derivation-drv-file-name))
+  "Alist used to define faces to highlight store file names.
+Each element of the list has a form:
+
+  (REGEXP . FACE)
+
+If any substring of the file name matches REGEXP, this file name
+will be highlighted with FACE.")
+
+(defun guix-derivation-file-name-face (file-name)
+  "Return a face to highlight FILE-NAME.
+See `guix-derivation-file-name-faces'."
+  (or (cdr (cl-find-if (lambda (assoc)
+                         (string-match-p (car assoc) file-name))
+                       guix-derivation-file-name-faces))
+      'guix-derivation-file-name))
 
 (defun guix-derivation-button (button)
   "View file Guix derivation BUTTON."
@@ -64,9 +95,13 @@
 (defun guix-derivation-make-buttons ()
   "Create buttons in the current Guix derivation buffer."
   (guix-while-search guix-derivation-file-regexp
-    (make-text-button (match-beginning 0)
-                      (match-end       0)
-                      :type            'guix-derivation-file)))
+    (let* ((beg    (match-beginning guix-derivation-file-regexp-group))
+           (end    (match-end       guix-derivation-file-regexp-group))
+           (string (substring-no-properties (match-string 1)))
+           (face   (guix-derivation-file-name-face string)))
+      (make-text-button beg end
+                        :type 'guix-derivation-file
+                        'font-lock-face face))))
 
 (defvar guix-derivation-mode-map
   (let ((map (make-sparse-keymap)))

commit e9e9e0212005cb4c8d8772891df127312de56ccf

elisp/derivation: Adjust regexp to find more files
File names are not necessarily enclosed in double quotes, they may also
be separated with spaces.

* elisp/guix-derivation.el (guix-derivation-file-regexp): Adjust to find
all file names.  Do not use any regexp group.
(guix-derivation-file-regexp-group): Change to 0.
(guix-derivation-make-buttons): Fix to use it (this micro-bug was
introduced by commit 7bc54679d148de866af56a0dc5ba1d02817f0eba).

elisp/guix-derivation.el
@@ -51,14 +51,12 @@
   :group 'guix-derivation-faces)
 
 (defcustom guix-derivation-file-regexp
-  (rx "\""
-      (group "/gnu/store/" (+ (not (any "\""))))
-      "\"")
+  (rx "/gnu/store/" (+ (not (any "\" "))))
   "Regexp matching Guix derivation file name."
   :type  'regexp
   :group 'guix-derivation)
 
-(defcustom guix-derivation-file-regexp-group 1
+(defcustom guix-derivation-file-regexp-group 0
   "Regexp group in `guix-derivation-file-regexp'."
   :type 'integer
   :group 'guix-derivation)
@@ -97,7 +95,8 @@ See `guix-derivation-file-name-faces'."
   (guix-while-search guix-derivation-file-regexp
     (let* ((beg    (match-beginning guix-derivation-file-regexp-group))
            (end    (match-end       guix-derivation-file-regexp-group))
-           (string (substring-no-properties (match-string 1)))
+           (string (substring-no-properties
+                    (match-string guix-derivation-file-regexp-group)))
            (face   (guix-derivation-file-name-face string)))
       (apply #'make-text-button
              beg end