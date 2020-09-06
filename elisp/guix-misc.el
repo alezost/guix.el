d246b2c443bbb9d49575b63bb78c14699c4448cf
Jan Nieuwenhuizen <janneke@gnu.org>
2020-09-06 10:46:46 +0200

    elisp/misc: Update Emacs path if needed
    This helps Geiser find the correct (environment's) Guile.
    
    * elisp/guix-misc.el (guix-set-emacs-environment): Set 'exec-path'
    when setting "PATH" variable.

--- elisp/guix-misc.el
+++ elisp/guix-misc.el
@@ -1,7 +1,7 @@
 ;;; guix-misc.el --- Miscellaneous definitions  -*- lexical-binding: t -*-
 
 ;; Copyright © 2014–2018 Alex Kost <alezost@gmail.com>
-;; Copyright © 2018 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+;; Copyright © 2018, 2020 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
 
 ;; This file is part of Emacs-Guix.
 
@@ -242,7 +242,9 @@ If PROFILE is nil, use `guix-current-profile'."
               (value (if (and separator current-value)
                          (concat path separator current-value)
                        path)))
-        (setenv variable value)))))
+        (setenv variable value)
+        (when (equalp variable "PATH")
+          (setq exec-path (split-string value ":")))))))
 
 
 ;;; Executing guix commands
